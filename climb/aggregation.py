abf58a8c2868e07e1070f1cdb7d27b1721ae9a1c
Bram Buysschaert <bram.buysschaert@gmail.com>
2022-11-25 11:01:39 -0500

    Include median computations

--- climb/aggregation.py
+++ climb/aggregation.py
@@ -4,6 +4,25 @@ import pandas as pd
 
 from typing import Literal
 
+def get_mediangrade(ds: pd.Series) -> str:
+    """
+    Determine the median grade of the logged ascents.  Assumes that the pandas.Series if of CategoricalDtype and is ordered (i.e., an ordinal measurement)
+
+    >>> df.groupby('week')['grade_french'].agg(get_mediangrade)
+    <some-answer>
+    >>> get_median_grade(df['grade_french'])
+    '6b+'
+    """
+    # Explicit median computation
+    grades = ds.sort_values().to_list()
+
+    if len(grades) % 2 == 0:
+        return grades[int(len(grades) / 2)]
+    elif len(grades) % 2 == 1:
+        return grades[int((len(grades) + 1) / 2)]
+    else:
+        return None
+
 def get_maxgrade_flash(df: pd.DataFrame, gradesystem: Literal['french', 'usa'] = 'french') -> str:
     """
     Determine the maximum grade of logged flash ascents.
